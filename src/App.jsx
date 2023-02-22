d03bb5e4f833e5e3ab1222c869192dd8a213f6fe
INXPIRE\s.rabosio <rabosio@it-sol.it>
2023-02-22 14:42:54 +0100

    callback assignment successfull!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,13 @@
-import { useState , useEffect , useRef, createRef} from 'react'
-import './App.css'
-import { findDOMNode } from "react-dom";
 import * as ebsol from '../zebra.it-sol.it/rfid'
 
 
   
 
 function App() {
-/*  const [domRef, setDomRef] = useState(null);
-  const [rfid,setRfid] = useState(null);
-  const [content, setContent] = useState("CIAO")
 
-
-  useEffect(() => {
-    const rfid = window.rfid;
-
-    console.log(rfid);
-
-  },[])
-
-  useEffect(() =>  {
-    console.log('useEffect [domRef]', domRef)
-    console.log(findDOMNode(domRef));
-      setRfid();
-      console.log('useEffect [domRef]', domRef)
-    
-  },[domRef]);
-
-
-  useEffect(() => {
-    console.log('useEffect [rfid]')
-    if (rfid) {
-
-    }
-  },[rfid])
-
-
-  function onEnum(e){
-
-    setContent("a) looking...")
-    console.log(e);
-
-    setContent("b) looking...")
-    rfid.enumerate();
-    setContent("c) looking...")
-  }
-*/
+  ebsol.onEnumerate(()=>{
+    alert("Hello from app")
+  })
 
   return (
     <>
--- zebra.it-sol.it/rfid.js
+++ zebra.it-sol.it/rfid.js
@@ -1,19 +1,32 @@
-export function enumerate()
-		{ 
-		  rfid.enumRFIDEvent = "enumRfid(%s);"
-		  rfid.enumerate();    
-		}
+
+
+
+
 window.enumRfid = (rfidArray)=>{ 
-            var rfidInfo = "RFID On Device: " + rfidArray.length + "--";  
+			var rfidInfo = "RFID On Device: " + rfidArray.length + "--"
 		  for (i=0; i < rfidArray.length; i++)
 		  {
-			rfidInfo = rfidInfo +"ID:"+ rfidArray[i][0] + '\nname:'+ rfidArray[i][1] + '\naddress '+rfidArray[i][2];
+			rfidInfo = rfidInfo +"ID:"+ rfidArray[i][0] + '\nname:'+ rfidArray[i][1] + '\naddress '+rfidArray[i][2]
 		  }
-          alert(rfidInfo)
+		  alert(rfidInfo)
+}
+
+/**
+ * Calls "onEnumerate" callback function and returns the number of rfid scanners
+ * @returns {number} number of rfid scanners found
+ */
+export function enumerate(){ 
+  rfid.enumRFIDEvent = "enumRfid(%s);"
+  return rfid.enumerate()
 }
 
 
-export const onEnumerate = ()=>{
 
+/**
+ * @function
+ * @param {function} callback - function that gets called during "enumerate()" execution
+ */
+export const onEnumerate = (callback)=>{
+	window.enumRfid = callback
 }
 
